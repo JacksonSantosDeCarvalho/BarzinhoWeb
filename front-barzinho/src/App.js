df308557029b7b2553a71cce3836e53d87cf2c7e
Jackson <jksantos200@gmail.com>
2023-06-23 12:51:15 -0300

    novas atualizações

--- front-barzinho/src/App.js
+++ front-barzinho/src/App.js
@@ -1,28 +1,18 @@
-import {useEffect,useState} from 'react';
 import './App.css';
-import CadastrarComanda from './CadastrarComanda';
-import ListarComanda from './ListarComanda';
+import Routess from './routes';
+
 
 
 function App() {
 
-  const [comandas, setComandas] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8080/comandas")
-      .then(retorno => retorno.json())
-      .then(retornoConvertidoEmJson =>
-        setComandas(retornoConvertidoEmJson))
-
-  }, [])//esse colchete é para realizar a requisição uma vez
-
   return (
     <div className="App">
-      <p>{JSON.stringify(comandas)}</p>
-      <CadastrarComanda></CadastrarComanda>
-      <ListarComanda lista={comandas}></ListarComanda>
+      <Routess></Routess>
     </div>
   );
 }
 
+
+
 export default App;
+
